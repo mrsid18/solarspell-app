6330b6693dd98bfad9052a62fa5971de7b810c8d
Suzan-Elmajali <74111783+Suzan-Elmajali@users.noreply.github.com>
2022-07-15 00:25:43 +0300

    update-searching, fix direction-rtl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost/backendi/',
+  apiUrl: 'http://localhost/backend/',
   contentUrl: 'http://localhost/content/',
 };
 
