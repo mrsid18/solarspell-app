64622a7381fb7daf9c3cf0d8c79de0bf7ee3423a
Joshua-Lubin <jlmortonr@gmail.com>
2022-07-14 13:52:27 -0700

    Added environment configuration for module path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost/backend/',
   contentUrl: 'http://www.solarcubed.org/dev-content-ea/',
+  moduleUrl: 'http://www.solarcubed.org/east-africa/',
 };
 
 /*
